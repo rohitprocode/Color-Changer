5245ad0cccae6c18d1927fbc9b53356f16afd7bd
rohitprocode <rohit990797@gmail.com>
2023-10-18 11:56:51 +0530

    Color

--- src/components/Colorinput.js
+++ src/components/Colorinput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Colorinput extends Component {
   constructor(props) {
     super(props)
-  
+    this.inputField = React.createRef()
     this.state = {
        colorValue : ""
     }
@@ -13,12 +13,16 @@ export default class Colorinput extends Component {
         colorValue : e.target.value
     })
   }
+
+  componentDidMount(){
+    this.inputField.current.focus()
+  }
   render() {
     return (
       <div style={{backgroundColor:this.state.colorValue}} className='input-area'>
+        <h1>Konsa Color</h1>
         <h1>{this.state.colorValue}</h1>
-        <input onChange={this.changeColor} type="text" />
-        <h2>{this.state.colorValue}</h2>
+        <input onChange={this.changeColor} type="text"  ref={this.inputField}  />
       </div>
     )
   }
